8100c534d30fa888a304792d7fff689c2d75c09b
Ben-Buli <84383897+Ben-Buli@users.noreply.github.com>
2021-12-06 15:28:04 +0800

    toogle ->text-dark

--- main-nav.php
+++ main-nav.php
@@ -2,7 +2,7 @@
 <nav class="sb-topnav navbar navbar-expand navbar-mao bg-main-cat ">
     <!-- Navbar 主圖示-->
     <div class="box-shift">
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0  sidebarToggle" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0  sidebarToggle text-dark" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
         <a class="navbar-brand admin-logo ps-0" href="index.php" title="首頁"><img src="images/admin-logo.png" alt=""></a>
     </div>
     <!--  上層選單  -->
