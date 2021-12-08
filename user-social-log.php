83788b891ecc507b21de42ff426087f718243473
Sinmei <93106691+Jonawonabanana@users.noreply.github.com>
2021-12-09 00:16:37 +0800

    Add files via upload

--- user-social-log.php
+++ user-social-log.php
@@ -41,14 +41,50 @@ $articleCount = $result->num_rows;
                     <li class="breadcrumb-item active">文章列表</li>
                 </ol>
 
+
+
+<!----------------------- card ---------------------->
+<!--                <div class="row">-->
+<!--                    <div class="col-xl-3 col-md-6">-->
+<!--                        <div class="card bg-primary text-white mb-4">-->
+<!--                            <div class="card-body">Primary Card</div>-->
+<!--                            <div class="card-footer d-flex align-items-center justify-content-between">-->
+<!--                                <a class="small text-white stretched-link" href="#">View Details</a>-->
+<!--                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="col-xl-3 col-md-6">-->
+<!--                        <div class="card bg-warning text-white mb-4">-->
+<!--                            <div class="card-body">Warning Card</div>-->
+<!--                            <div class="card-footer d-flex align-items-center justify-content-between">-->
+<!--                                <a class="small text-white stretched-link" href="#">View Details</a>-->
+<!--                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="col-xl-3 col-md-6">-->
+<!--                        <div class="card bg-success text-white mb-4">-->
+<!--                            <div class="card-body">Success Card</div>-->
+<!--                            <div class="card-footer d-flex align-items-center justify-content-between">-->
+<!--                                <a class="small text-white stretched-link" href="#">View Details</a>-->
+<!--                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+
+
+
+
                 <!-- 副標題 end -->
                 <div class="card mb-4">
                     <div class="card-header">
                         <i class="fas fa-table me-1"></i>
                         社群文章列表
                     </div>
-
-
 
                     <!-- 表格區塊 -->
                     <div class="card-body">
@@ -66,7 +102,6 @@ $articleCount = $result->num_rows;
                             </tr>
                             </thead>
 
-
                             <!-- 總結資訊 tfoot -->
                             <tfoot>
 <!--                            <tr>-->
@@ -77,6 +112,7 @@ $articleCount = $result->num_rows;
 <!--                                <th>其他操作</th>-->
 <!--                            </tr>-->
                             </tfoot>
+
                             <!-- 資料欄 tbody -->
                             <tbody>
                             <?php if($articleCount>0 ):
@@ -89,13 +125,16 @@ $articleCount = $result->num_rows;
                                         <td><?=$row["article_cate"]?></td>
                                         <td><?=$row["created_at"]?></td>
                                         <td>
-                                            <a class="btn btn-mao-primary " href="user.php?id=<?=$row["id"]?>"><i
-                                                    class="fas fa-user" title="檢視此會員資料"></i></a>
                                             <a class="btn btn-mao-primary "
-                                               href="user.php?id=<?= $row["id"] ?>"><i class="fas fa-edit"></i></a>
-
-                                            <a class="btn btn-mao-primary " href="user-order.php?id=<?=$row["id"]?>"><i
-                                                    class="fas fa-credit-card " title="會員訂單"></i></a>
+                                               href="user-social-edit.php?id=<?= $row["id"] ?>">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a class="btn btn-mao-primary "
+                                               href="user-social-delete.php?id=<?=$row["id"]?>">
+                                                <i class="fas fa-trash-alt"></i>
+                                            </a>
                                         </td>
                                     </tr>
                                 <?php endwhile; ?>
