801313075fa494cefb3bed70c07ed5b5db3c6524
dummiss <66296723+dummiss@users.noreply.github.com>
2021-12-10 05:43:59 +0800

    萱-更新

--- cart-shipping.php
+++ cart-shipping.php
@@ -13,6 +13,35 @@ if (empty($_SESSION['cart'])) {
     $cartCount = count($_SESSION['cart']);
 }
 
+//搜尋使用者資料
+
+$account="user";
+if (isset($_POST["useraccount"])) {
+    $useraccount= $_POST["useraccount"];
+
+}
+$sql = "SELECT * FROM users WHERE account=?  AND valid=1";
+$stmt = $db_host->prepare($sql);
+try {
+    $stmt->execute([$useraccount]);
+    $userExist = $stmt->rowCount();
+//    echo $userExist;
+    if ($userExist > 0) {
+        $row = $stmt->fetch();
+        $id = $row["id"];
+        $account=$row["account"];
+        $name = $row["mailing_name"];
+        $email = $row["mailing_email"];
+        $phone = $row["mailing_phone"];
+        $address = $row["mailing_address"];
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+if(isset($useraccount)&&$useraccount===$account){
+ $a="yes";
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -77,6 +106,7 @@ if (empty($_SESSION['cart'])) {
             box-shadow: 12px 12px 7px rgba(0, 0, 0, 0.4);
             margin-left: 60px;
             margin-top: 80px;
+
         }
 
     </style>
@@ -91,7 +121,7 @@ if (empty($_SESSION['cart'])) {
     <div class="container px-0 mt-5">
         <div class="main px-5">
             <header class="d-flex justify-content-between align-items-center mb-3">
-                <h1 class="title fs-2">購物車清單</h1>
+                <h1 class="title fs-2">購物車清單<?=$a?></h1>
                 <a class="link-secondary" href="cart-shipping.php"><img class="CartIcon"
                                                                         src="images/shopping-bag.png"><span><?= $cartCount ?></span></a>
             </header>
@@ -113,7 +143,7 @@ if (empty($_SESSION['cart'])) {
                     <tbody>
                     <?php
                     $totalPrice = 0;
-                    $deliveryFee=60; //運費
+                    $deliveryFee = 60; //運費
                     if (!empty($_SESSION["cart"])):
                         $cart = array();
                         $cart = $_SESSION["cart"];
@@ -144,29 +174,36 @@ if (empty($_SESSION['cart'])) {
 
                 <div class="row m-0 mb-5 ">
                     <div class="col-5 p-0">
-                        <form class="d-grid gap-3" action="">
-                            <h2 class="fs-4 border-bottom pb-2 mb-3 fw-bold">帳單資訊</h2>
+                        <h2 class="fs-4 border-bottom pb-2 mb-3 fw-bold">帳單資訊</h2>
+                        <form class="d-grid gap-3 mb-3" action="" method="post" name="form1">
                             <div>
-                                <lebel class="text-danger">請輸入會員帳號</lebel>
-                                <input class="form-control form-control" type="text"
-                                       placeholder="type your account name">
+                                <label for="useraccount" class="text-danger">請輸入會員帳號</label>
+                                <input  <?php if(isset($useraccount)&&$useraccount===$account):?> value="<?=$account?>" <?php endif;?> name="useraccount" type="text" class="form-control form-control" required >
+                                <button id="yes" class="btn btn-mao-primary mt-2" type="submit" name="Submit">確認</button>
+
+
+                            </div>
+                        </form>
+                        <form class="d-grid gap-3" action="" method="post" name="form2">
+                            <?php if(isset($useraccount)&&$useraccount===$account):?>
+                            <div>
+                                <label for="name">聯絡人</label>
+                                <input value="<?=$name?>" name="name" type="text" class="form-control form-control" placeholder="name" required>
                             </div>
                             <div>
-                                <lebel>姓名</lebel>
-                                <input class="form-control form-control" type="text" placeholder="name">
+                                <label for="validationCustom03" class="form-label">詳細地址</label>
+                                <input value="<?=$address?>" type="text" class="form-control" id="validationCustom03" required>
+                                <div class="invalid-feedback">請填入詳細地址</div>
                             </div>
                             <div>
-                                <lebel>地址</lebel>
-                                <input class="form-control form-control" type="text" placeholder="address">
+                                <label for="phone">電話</label>
+                                <input value="<?=$phone?>" name="phone" type="tel" class="form-control form-control">
                             </div>
                             <div>
-                                <lebel>電話</lebel>
-                                <input class="form-control form-control" type="text" placeholder="phone">
+                                <label for="email">Email</label>
+                                <input value="<?=$email?>" name="email" class="form-control form-control" type="email">
                             </div>
-                            <div>
-                                <lebel>Email</lebel>
-                                <input class="form-control form-control" type="text" placeholder="email">
-                            </div>
+                            <?php endif;?>
 
                         </form>
                     </div>
@@ -180,11 +217,12 @@ if (empty($_SESSION['cart'])) {
                                         <div>結帳金額：</div>
                                     </div>
                                     <div class="col-2 d-grid gap-3">
-                                        <?php if ($totalPrice == 0){
-                                            $deliveryFee=0;}
+                                        <?php if ($totalPrice == 0) {
+                                            $deliveryFee = 0;
+                                        }
                                         ?>
                                         <div>NT$<?= $totalPrice ?></div>
-                                        <div>+NT$<?=$deliveryFee ?></div>
+                                        <div>+NT$<?= $deliveryFee ?></div>
                                         <div>NT$<?= $totalPrice + $deliveryFee ?></div>
                                     </div>
                                 </div>
@@ -205,9 +243,9 @@ if (empty($_SESSION['cart'])) {
 
 <?php require_once("JS.php"); ?>
 
-<script>//安全檢查刪除購物車
+<script>
+    //安全檢查刪除購物車
     let count = '<?=$cartCount?>';//接住php變數
-
     if (count == 0) {  //判斷購物車是否為0
         stop()
     } else {
@@ -217,9 +255,24 @@ if (empty($_SESSION['cart'])) {
             if (window.confirm('是否要全部刪除?') == true) {
                 document.location.href = "cartCleanAll.php?url=" + location.href;
             }
-        };
+        }
         cleanAll.addEventListener('click', doClean);
     }
+
+    //
+    //var button = document.querySelector("#yes");
+
+    //
+    //
+    //button.onclick = function () {
+    //    alert("只限會員購買，請註冊會員");
+    //};
+
+    let button = document.querySelector("#yes");
+    let userkeyin='<?=$useraccount?>';
+    let account='<?=$account?>';
+
+
 </script>
 </body>
 </html>
