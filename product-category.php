e408a70190713ddb431f801cd68b7cd60f8d8b00
dummiss <66296723+dummiss@users.noreply.github.com>
2021-12-11 00:51:22 +0800

    萱-更新

---
+++ product-category.php
@@ -0,0 +1,47 @@
+<?php
+require_once("pdo-connect.php"); //連線到本地資料庫
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="Description" content="MaoBook小組專題報告"/>
+    <meta name="Content-Language" content="zh-TW">
+    <meta name="author" content="Team MaoBook"/>
+    <!--  網站圖示  -->
+    <link rel="apple-touch-icon" type="image/png" href="images/logo-nbg.png"/>
+    <link rel="shortcut icon" type="image/png" href="images/logo-nbg.png"/>
+    <link rel="mask-icon" type="image/png" href="images/logo-nbg.png"/>
+
+    <title>板模</title>
+
+<!--  板模的css、BS5、fontawesome  -->
+    <?php require_once("style.php"); ?>
+    <!--  板模end  -->
+
+    <!-- 此處新增css檔 -->
+    <!-- Css檔 end   -->
+
+
+</head>
+<body class="sb-nav-fixed">
+<?php require_once("main-nav.php"); ?>
+<!-- 主要內容 -->
+<div id="layoutSidenav_content">
+    <div class="container px-0">
+        <main class="main px-5">
+
+            product_catrgory.php
+
+        </main><!-- 主要內容end -->
+
+    </div>
+<!--    --><?php //require_once("footer.php"); ?>
+
+</div>
+<?php require_once("JS.php"); ?>
+</body>
+</html>
