96661768d4f22970a4a7783f1e7752575097dca5
Eutene <95270417+Eutene@users.noreply.github.com>
2021-12-10 21:47:43 +0800

    年更新 圖片路徑

--- create-user.php
+++ create-user.php
@@ -46,7 +46,7 @@
             <form action="doSignUp.php" method="post"  >
 
                 <div class="form-group"></div>
-                <img class="logo" src="/images/logo.png" alt="">
+                <img class="logo" src="images/logo.png" alt="">
                 <div class="progress mb-3">
                     <div class="progress-bar bg-warning " role="progressbar" style="width: 100%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
