0df9a305b65a1eb18612f06232a2c28cedd80fde
Eutene <95270417+Eutene@users.noreply.github.com>
2021-12-11 22:40:38 +0800

    su06

--- doSignUp.php
+++ doSignUp.php
@@ -1,34 +1,44 @@
 <?php
 require_once ("pdo-connect.php");
+$account=$_POST["account"];
 $name=$_POST["name"];
 $email=$_POST["email"];
 $mobile=$_POST["mobile"];
 $password=$_POST["password"];
-$repassword=$_POST["repassword"];
+$RePassword=$_POST["RePassword"];
+$valid=1;
 
 
-if($password!==$repassword){
-    echo "密碼不一致";
+if($password!==$RePassword){
+
+    echo '<div class="alert alert-warning" role="alert">
+        密碼不一致</div>';
     exit();
 }
 $crPassword=md5($password);
 //echo "$crPassword<br>";
-$sqlCheck="SELECT * FROM users WHERE email='$email'";
+$sqlCheck="SELECT * FROM users WHERE account='$account'";
 $checkResult=$conn->query($sqlCheck);
 $emailExist=$checkResult->num_rows;
 //echo $userExist;
 if($emailExist>0){
-    echo "已有相同信箱進行註冊";
+
+    echo '<div class="alert alert-warning" role="alert">
+        已有相同信箱進行註冊！</div>';
     exit();
+
 }
 $now=date("Y-m-d H:i:s");
-$sql="INSERT INTO users(name, email, mobile, password, created_at) VALUES('$name', '$email','$mobile', '$crPassword', '$now')";
+$sql="INSERT INTO users( account , name , email, mobile , password , valid , created_at)VALUES('$account','$name', '$email','$mobile', '$password','$valid','$now')";
 
 
 if ($conn->query($sql) === TRUE) {
-    echo "新增資料完成<br>";
+    echo '<div class="alert alert-warning" role="alert">
+        註冊成功！期待與您一起書寫毛毛日記！</div>';
     $id=$conn->insert_id;
     header("location: sign-in.php");
+
+
 } else {
     echo "新增資料錯誤: " . $conn->error;
 }
