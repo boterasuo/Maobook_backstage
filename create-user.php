e18d44b332277e8946a56351cae2c5637014f8bd
Eutene <95270417+Eutene@users.noreply.github.com>
2021-12-12 03:40:00 +0800

    年更新

--- create-user.php
+++ create-user.php
@@ -43,41 +43,83 @@
   </head>
   <body class="d-flex justify-content-center align-items-center vh-200">
         <div class="login-panel text-center">
-            <form action="doSignUp.php" method="post" class="needs-validation" novalidate >
-
+            <form class="needs-validation" action="doSignUp.php" method="post"  novalidate >
                 <div class="form-group"></div>
                 <img class="logo" src="images/logo.png" alt="">
                 <div class="progress mb-3">
                     <div class="progress-bar bg-warning " role="progressbar" style="width: 100%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
 
-                    <div class="form-floating input mb-2">
-                        <input type="text" class="form-control" id="name" placeholder="name" name="name" required>
-                        <label for="name">姓名</label>
-                    </div>
-                    <div class="form-floating input mb-2">
-                        <input id="account" type="text" name="account" class="form-control"  placeholder="account" required>
-                        <label for="account">帳號</label>
-                    </div>
-                    <div class="form-floating input mb-2">
-                        <input type="email" class="form-control" id="email" placeholder="name@example.com" name="email" data-error="郵件格式錯誤">
-                        <label for="email">信箱</label>
-                    </div>
-                    <div class="form-floating input mb-2">
-                        <input type="mobile" class="form-control" id="mobile" placeholder="mobile" name="mobile">
-                        <label for="mobile">手機</label>
+                    <div class="form-floating input mb-3">
+                        <input type="text" class="form-control" id="validationDefault01" placeholder="name" name="name" required>
+                        <label for="validationDefault01">姓名</label>
+                        <div class="col-auto">
+    <span id="passwordHelpInline" class="form-text">
+      請輸入您的中文姓名
+    </span>
+                        </div>
                     </div>
 
-                <div class="form-floating input mb-2">
-                    <input type="password" class="form-control" id="password"  name="password" placeholder="設定密碼" pattern="^(?=.*\d)(?=.*[a-zA-Z]).{6,}$" data-error="請輸入含有英文字母及數字的密碼，至少六個字元。" >
-                    <label for="password">密碼，請輸入至少六字元的字母與數字</label>
+
+                <div class="form-floating input mb-3">
+                        <input id="validationCustom02" type="text" name="account" class="form-control"  placeholder="account" pattern="^(?=.*\d)(?=.*[a-zA-Z]).{6,}$" required>
+                        <label for="validationCustom02">帳號</label>
+                    <div class="col-auto">
+    <span id="passwordHelpInline" class="form-text">
+      請設置至少六個字元的帳號，需要包含字母與數字
+    </span>
+                    </div>
+
+                    </div>
+                    <div class="form-floating input mb-3">
+                        <input type="email" class="form-control" id="validationCustom03" placeholder="name@example.com" name="email" pattern="/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/">
+                        <label for="validationCustom03">信箱</label>
+                        <div class="col-auto">
+    <span id="passwordHelpInline" class="form-text">
+      請輸入有效的電子信箱
+    </span>
+                        </div>
+                    </div>
+                    <div class="form-floating input mb-3">
+                        <input type="tel" class="form-control" id="validationCustom04" placeholder="mobile" name="mobile" pattern="^09\d{2}-\d{3}-\d{3}$">
+                        <label for="validationCustom04">手機</label>
+                        <div class="col-auto">
+    <span id="passwordHelpInline" class="form-text">
+      請輸入正確的手機格式九碼，如0900-000-000
+    </span>
+                        </div>
+                    </div>
+
+
+
+                <div class="form-floating input mb-3">
+                    <input type="password" class="form-control" id="validationCustom05"  name="password" placeholder="設定密碼" pattern="^(?=.*\d)(?=.*[a-zA-Z]).{6,}$"  >
+                    <label for="validationCustom05">密碼</label>
+                    <div class="col-auto">
+    <span id="passwordHelpInline" class="form-text">
+      請輸入至少六個字元，需要包含字母與數字
+    </span>
+                    </div>
                 </div>
 
-                <div class="form-floating input mb-2">
-                    <input id="RePassword" name="RePassword" class="form-control" type="password" placeholder="確認密碼" data-match="#inputPassword" data-error="密碼未吻合！" required="required">
-                    <label for="RePassword">確認密碼</label>
-                    <div class="help-block with-errors"></div>
+                <div class="form-floating input mb-3">
+                    <input id="validationCustom06" name="RePassword" class="form-control" type="password" placeholder="確認密碼" data-match="#inputPassword" data-error="密碼未吻合！" required="required">
+                    <label for="validationCustom06">確認密碼</label>
+                    <div class="col-auto">
+    <span id="passwordHelpInline" class="form-text">
+      需與上述密碼相同，請注意大小寫
+    </span>
+                    </div>
                 </div>
+
+                <div class="col-12 mb-5 mt-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+                        <label class="form-check-label" for="invalidCheck2">
+                            Agree to terms and conditions
+                        </label>
+                    </div>
+
                 <div class="mb-3">
                     <input id="valid" type="hidden" name="valid" class="form-control" >
                 </div>
@@ -88,11 +130,38 @@
                   </div>
                   <div class="text-muted">© maobook 2021</div>
             </div>
+
+
             </form>
+
+
         </div>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+        <script>
+            // Example starter JavaScript for disabling form submissions if there are invalid fields
+            (function () {
+                'use strict'
+
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.querySelectorAll('.needs-validation')
+
+                // Loop over them and prevent submission
+                Array.prototype.slice.call(forms)
+                    .forEach(function (form) {
+                        form.addEventListener('submit', function (event) {
+                            if (!form.checkValidity()) {
+                                event.preventDefault()
+                                event.stopPropagation()
+                            }
+
+                            form.classList.add('was-validated')
+                        }, false)
+                    })
+            })()
+        </script>
 
 
 
