a121420116a853388b7a92410c9ac75677148522
Sinmei <93106691+Jonawonabanana@users.noreply.github.com>
2021-12-12 15:24:06 +0800

    美-更新

--- social-doDelete.php
+++ social-doDelete.php
@@ -1,10 +1,15 @@
 <?php
-require_once("../db-connect.php");
 
 $id = $_GET["id"];
 
+require_once("db-connect.php");
+
+require_once("style.php");
+require_once("main-nav.php");
+
 //$sql = "DELETE FROM users WHERE id='$id'";
 $sql = "UPDATE social_forum SET valid=9 WHERE id='$id'";
+
 
 if ($conn->query($sql) === TRUE) {
     echo "刪除資料完成<br>";
@@ -18,4 +23,4 @@ if ($conn->query($sql) === TRUE) {
     <a class="btn btn-info" href="user-social-log.php">返回文章列表</a>
 </div>
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">-->
