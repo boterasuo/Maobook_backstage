76f63c89a49bedfc2ec5c76c9d05530954b48f79
Sinmei <93106691+Jonawonabanana@users.noreply.github.com>
2021-12-12 15:23:11 +0800

    美-更新

--- user-social-log.php
+++ user-social-log.php
@@ -1,7 +1,23 @@
 <?php
 require_once("db-connect.php");//連線資料庫;
+if(isset($_GET["id"])){
+    $id=$_GET["id"];
+}else{
+    $id=0;
+}
+
 $sql = "SELECT * FROM social_forum WHERE valid=1";
-//$sql="SELECT id, account, name FROM users";
+
+
+//關聯式資料庫 連結forum_category 和 social_forum/category
+$sqlCategory="SELECT * FROM forum_category";
+$resultCategory=$conn->query($sqlCategory);
+//["1"=> "marvel", "2"=>"DC Comics"];
+$categoryArr=[];
+while($row=$resultCategory->fetch_assoc()){
+    $categoryArr[$row["id"]]=$row["name"];
+}
+
 
 $result = $conn->query($sql);
 $articleCount = $result->num_rows;
@@ -23,7 +39,7 @@ $articleCount = $result->num_rows;
     <link rel="shortcut icon" type="image/png" href="images/logo-nbg.png"/>
     <link rel="mask-icon" type="image/png" href="images/logo-nbg.png"/>
 
-    <title><?=$rowUser["name"]?> 的社群紀錄</title>
+    <title><?=$rowUser["name"]?> 貓貓的社群紀錄</title>
 
     <?php require_once("style.php"); ?>
 
@@ -38,46 +54,11 @@ $articleCount = $result->num_rows;
     <div class="container px-0">
         <main class="main px-5">
             <div class="container-fluid px-4">
-                <h1 class="mt-4"><?=$rowUser["name"]?>OO的社群紀錄</h1>
+                <h1 class="mt-4"><?=$rowUser["name"]?> 貓貓的社群紀錄</h1>
                 <ol class="breadcrumb mb-4">
                     <li class="breadcrumb-item"><a href="home.php">首頁</a></li>
                     <li class="breadcrumb-item active">文章列表</li>
                 </ol>
-
-<!----------------------- card ---------------------->
-<!--                <div class="row">-->
-<!--                    <div class="col-xl-3 col-md-6">-->
-<!--                        <div class="card bg-primary text-white mb-4">-->
-<!--                            <div class="card-body">Primary Card</div>-->
-<!--                            <div class="card-footer d-flex align-items-center justify-content-between">-->
-<!--                                <a class="small text-white stretched-link" href="#">View Details</a>-->
-<!--                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!---->
-<!--                    <div class="col-xl-3 col-md-6">-->
-<!--                        <div class="card bg-warning text-white mb-4">-->
-<!--                            <div class="card-body">Warning Card</div>-->
-<!--                            <div class="card-footer d-flex align-items-center justify-content-between">-->
-<!--                                <a class="small text-white stretched-link" href="#">View Details</a>-->
-<!--                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!---->
-<!--                    <div class="col-xl-3 col-md-6">-->
-<!--                        <div class="card bg-success text-white mb-4">-->
-<!--                            <div class="card-body">Success Card</div>-->
-<!--                            <div class="card-footer d-flex align-items-center justify-content-between">-->
-<!--                                <a class="small text-white stretched-link" href="#">View Details</a>-->
-<!--                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-
-
 
 
                 <!-- 副標題 end -->
@@ -116,9 +97,9 @@ $articleCount = $result->num_rows;
                                     ?>
                                     <tr>
                                         <td><?=$row["id"]?></td>
-                                        <td><?=$row["article_title"]?></td>
-                                        <td><?=$row["article_content"]?></td>
-                                        <td><?=$row["article_cate"]?></td>
+                                        <td><?=$row["title"]?></td>
+                                        <td><?=$row["content"]?></td>
+                                        <td><?=$row["category"]?></td>
                                         <td><?=$row["created_at"]?></td>
                                         <td><!--編輯 -->
                                             <a class="btn btn-mao-primary "
